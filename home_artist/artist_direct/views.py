49d1e1c1cd1beeaac6f54ec7110924c9bece1660
Glenn Langdon <glennlangdon@gmail.com>
2020-07-29 14:40:05 -0600

    User model migrate

--- home_artist/artist_direct/views.py
+++ home_artist/artist_direct/views.py
@@ -1,7 +1,31 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
     return render(request, "artist_direct/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(
+                request,
+                "network/login.html",
+                {"message": "Invalid username and/or password."},
+            )
+    else:
+        return render(request, "network/login.html")
+
+
 
 
