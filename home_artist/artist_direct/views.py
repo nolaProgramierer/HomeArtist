142a571e4191ea741bfa25850785bb1f406f6899
Glenn Langdon <glennlangdon@gmail.com>
2020-07-29 13:54:52 -0600

    Artist index page render

--- home_artist/artist_direct/views.py
+++ home_artist/artist_direct/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Artist Direct is here!")
+    return render(request, "artist_direct/index.html")
 
 
