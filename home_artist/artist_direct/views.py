8dfd44714998fd20f5f6ca3c53d0a368de37e75e
Glenn Langdon <glennlangdon@gmail.com>
2020-07-29 15:06:05 -0600

    Login and redirect to index

--- home_artist/artist_direct/views.py
+++ home_artist/artist_direct/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db import IntegrityError
+
+from .models import User
 
 def index(request):
     return render(request, "artist_direct/index.html")
@@ -20,11 +24,43 @@ def login_view(request):
         else:
             return render(
                 request,
-                "network/login.html",
+                "artist_direct/login.html",
                 {"message": "Invalid username and/or password."},
             )
     else:
-        return render(request, "network/login.html")
+        return render(request, "artist_direct/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(
+                request, "artist_direct/register.html", {"message": "Passwords must match."}
+            )
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(
+                request, "artist_direct/register.html", {"message": "Username already taken."}
+            )
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "artist_direct/register.html")
+
 
 
 
