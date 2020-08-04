0f84fdebfe74f578239d83661a435a9cc1b37e96
Glenn Langdon <glennlangdon@gmail.com>
2020-08-03 23:26:22 -0600

    urlize

---
+++ artist_direct/static/artist_direct/scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // Comment text and submit button 
+    const submit = document.querySelector("#submit");
+    const newText = document.querySelector("#text");
+
+    // Comment form listener
+    document.querySelector("#comment-form").onsubmit = () => {
+
+        //Add comment and rating to db
+
+    }
+
+});
