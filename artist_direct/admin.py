101cd8d71c11d4135d0bf2a4cde34e8526e21064
Glenn Langdon <glennlangdon@gmail.com>
2020-08-04 00:54:04 -0600

    Good night

--- artist_direct/admin.py
+++ artist_direct/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Profile, Image
+from .models import User, Profile, Image, Comment
 
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Image)
+admin.site.register(Comment)
 
 
