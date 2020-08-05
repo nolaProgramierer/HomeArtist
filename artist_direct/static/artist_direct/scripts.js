ef6b6fb60165c08ef9f33184d0bbbd108685e244
Glenn Langdon <glennlangdon@gmail.com>
2020-08-05 13:22:17 -0600

    Lint for JS file

--- artist_direct/static/artist_direct/scripts.js
+++ artist_direct/static/artist_direct/scripts.js
@@ -2,11 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Retrieve selector of comment element
     const comment_id = document.querySelector("#comment-text");
- 
-    document.querySelector("#submit").addEventListener("click", function (event) {     
+
+    document.querySelector("#submit").addEventListener("click", function (event) {
         addComment(comment_id.dataset.profileId);
-        console.log("This is after the submit function call.");
-        event.preventDefault();      
+        event.preventDefault();
     });
 
     console.log("DOM content loaded");
@@ -17,10 +16,8 @@ function addComment(profile_id) {
     // Get values from comment field
     const comment_txt = document.querySelector("#comment-text").value;
     const rating_txt = document.querySelector("#rating").value;
-    const data = {comment:comment_txt, rating:rating_txt};
+    const data = { comment: comment_txt, rating: rating_txt };
     const token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-
-    console.log(profile_id);
 
     fetch(`/add_comment/${profile_id}`, {
         method: 'POST',
@@ -33,17 +30,15 @@ function addComment(profile_id) {
         .then(response => response.json())
         .then(data => {
             console.log('Success:', data);
-            
+            // Add comment to artist profile page
             let node = document.createElement("P");
             let textnode = document.createTextNode(data.comment);
             node.appendChild(textnode);
             document.querySelector("#comment-box").appendChild(node);
-            
+            // Add rating to artist profile page
             let node1 = document.createElement("P");
             let textnode1 = document.createTextNode(data.rating);
             node.appendChild(textnode1);
             document.querySelector("#comment-box").appendChild(node1);
-            //document.querySelector("#comment-box").appendChild(document.createTextNode(data.comment));
-            //document.querySelector("#rating-box").appendChild(document.createTextNode(data.stars));
         });
 }// End addComment
